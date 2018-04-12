7ad59ca422a2d81e97d79ef2ec9006db473b3b7c
Lennart Vogelsang <dev@vogelsangs.net>
2018-04-12 14:30:50 +0200

    commited changes from previous commit

--- src/communication/corrupted.rs
+++ src/communication/corrupted.rs
@@ -22,6 +22,16 @@ impl <S: Default, C: Read + Write> CorruptedChannel<S, C> {
         CorruptedChannel {state: S::default(), conn: conn, eavesdropper: empty_eavesdropper, corruptor: empty_corruptor}
     }
     
+    pub fn new_eavesdrop(conn: C, initial_state: S, 
+    eavesdropper: fn(&mut S, &[u8])) -> Self {
+        CorruptedChannel {state: S::default(), conn: conn, eavesdropper: eavesdropper, corruptor: empty_corruptor}
+    }
+
+    pub fn new_corrupt(conn: C, initial_state: S, 
+    corruptor: fn(&mut S, &mut [u8])) -> Self {
+        CorruptedChannel {state: S::default(), conn: conn, eavesdropper: empty_eavesdropper, corruptor: corruptor}
+    }
+    
     pub fn new(conn: C, initial_state: S, 
     eavesdropper: fn(&mut S, &[u8]), corruptor: fn(&mut S, &mut [u8])) -> Self {
         CorruptedChannel {state: S::default(), conn: conn, eavesdropper: eavesdropper, corruptor: corruptor}
@@ -31,14 +41,18 @@ impl <S: Default, C: Read + Write> CorruptedChannel<S, C> {
 impl <S: Default, C: Read + Write> Read for CorruptedChannel<S, C> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
         (self.corruptor)(&mut self.state, buf);
-        self.conn.write(buf)
+        self.conn.read(buf)
     }
 }
 
-impl <S: Default, C: Read + Write> Read for CorruptedChannel<S, C> {
-    fn write(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
-        (self.corruptor)(&mut self.state, buf);
+impl <S: Default, C: Read + Write> Write for CorruptedChannel<S, C> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        (self.eavesdropper)(&mut self.state, buf);
         self.conn.write(buf)
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        self.conn.flush()
     }
 }
 
